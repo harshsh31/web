8f337e03d727e9646221f0981188fb6f5cab4fd2
harshsh31 <harshsh31@gmail.com>
2018-04-13 09:42:44 +0530

    email settings

--- anil/settings.py
+++ anil/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'harshsh31@gmail.com' 
+EMAIL_HOST_PASSWORD = 'harshshah31'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL= EMAIL_HOST_USER
 
 # Application definition
 
